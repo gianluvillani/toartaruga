047fb2b3b3b6753db9ccf8c53730ffb7c7a5e49a
jonathan <jnthnj@kth.se>
2018-11-13 12:41:51 +0100

    Added idle node.

--- supervisor/src/supervisor.py
+++ supervisor/src/supervisor.py
@@ -4,7 +4,43 @@ import rospy
 import smach
 import smach_ros
 from std_msgs.msg import Float32
+from nav_msgs.msg import Path
 import threading
+
+class StateIdle(smach.State):
+
+	def __init__(self):
+		smach.State.__init__(self, outcomes=['stopped', 'start'])
+		self.sub_danger = rospy.Subscriber('/obstacles/danger', Float32, self.danger_callback)
+		self.sub_path = rospy.Subscriber('/SVEA2/path', Path, self.path_callback)
+		self.mutex = threading.Lock()
+		self.danger = 0.0
+		self.threshold = 0.8
+		self.sleep_time = 0.1
+		self.path = None
+
+	def execute(self, ud):
+		while True:
+			self.mutex.acquire()
+			if self.path != None:
+				if self.danger > self.threshold:
+					self.mutex.release()
+					return 'stopped'
+				else:
+					self.mutex.release()
+					return 'start'
+			self.mutex.release()
+			rospy.sleep(self.sleep_time)
+
+	def path_callback(self, msg):
+		self.mutex.acquire()
+		self.path = msg
+		self.mutex.release()
+
+	def danger_callback(self, msg):
+		self.mutex.acquire()
+		self.danger = msg
+		self.mutex.release()
 
 class StateRunning(smach.State):
 
@@ -16,13 +52,15 @@ class StateRunning(smach.State):
 		self.threshold = 0.8
 		self.sleep_time = 0.1
 
+
 	def execute(self, ud):
-		self.mutex.acquire()
-		if self.danger > self.threshold:
-			return 'stop'
-		self.mutex.release()
-		rospy.sleep(self.sleep_time)
-		return 'running'
+		while True:
+			self.mutex.acquire()
+			if self.danger > self.threshold:
+				self.mutex.release()
+				return 'stop'
+			self.mutex.release()
+			rospy.sleep(self.sleep_time)
 
 	def callback(self, msg):
 		self.mutex.acquire()
@@ -40,12 +78,12 @@ class StateStopped(smach.State):
 		self.sleep_time = 0.1
 
 	def execute(self, ud):
-		self.mutex.acquire()
-		if self.danger < self.threshold:
-			return 'start'
-		self.mutex.release()
-		rospy.sleep(self.sleep_time)
-		return 'stopped'
+		while True:
+			self.mutex.acquire()
+			if self.danger < self.threshold:
+				return 'start'
+			self.mutex.release()
+			rospy.sleep(self.sleep_time)
 
 	def callback(self, msg):
 		self.mutex.acquire()
@@ -57,8 +95,11 @@ if __name__ == "__main__":
 	rospy.init_node('supervisor', anonymous=True)
 	sm = smach.StateMachine(outcomes=[])
 	with sm:
-		smach.StateMachine.add('StateRunning', StateRunning(), transitions={'running':'StateRunning', 'stop':'StateStopped'})
-		smach.StateMachine.add('StateStopped', StateStopped(), transitions={'stopped':'StateStopped', 'start':'StateRunning'})
-
+		smach.StateMachine.add('StateIdle', StateIdle(),
+		                       transitions={'start': 'StateRunning', 'stopped': 'StateStopped'})
+		smach.StateMachine.add('StateRunning', StateRunning(),
+		                       transitions={'running':'StateRunning', 'stop':'StateStopped'})
+		smach.StateMachine.add('StateStopped', StateStopped(),
+		                       transitions={'stopped':'StateStopped', 'start':'StateRunning'})
 	outcome = sm.execute()
 
