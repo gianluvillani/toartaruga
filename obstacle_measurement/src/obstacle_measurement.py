da7424443089d0fb3a1afa10a242b4226b68560c
toartaruga <group2@kth.se>
2018-11-27 09:40:57 +0000

    Segment detection is working

--- obstacle_measurement/src/obstacle_measurement.py
+++ obstacle_measurement/src/obstacle_measurement.py
@@ -45,25 +45,34 @@ class obstacle_measurement:
 			False
 	def publish_danger(self):
 		danger_msg = Float32()
-		obstacle_found = False
+		circle_obstacle_found = False
+		segment_obstacle_found = False
 		for circle in self.circle_obstacles:
 			if self.circle_collision(circle):
-				obstacle_found = True
-		if obstacle_found:
+				circle_obstacle_found = True
+		for segment in self.segment_obstacles:
+			if self.segment_collision(segment):
+				segment_obstacle_found = True
+		if circle_obstacle_found or segment_obstacle_found:
 			danger_msg.data = 1
 		else:		
 			danger_msg.data = 0
 		self.pub_danger.publish(danger_msg)
 
 		
+	def segment_collision(self, segment_obstacle): # A segment_obstacle is a list [(first_point_x, first_point_y), (last_point_x, last_point_y)]
+		first_point_x = segment_obstacle[0][0]
+		first_point_y = segment_obstacle[0][1]
+		last_point_x = segment_obstacle[1][0]
+		last_point_y = segment_obstacle[1][1]
+		middle_point_x = (first_point_x + last_point_x)/2
+		middle_point_y = (first_point_y + last_point_y)/2
 
-	def segment_distance(self, segment_obstacle): # A segment_obstacle is a list [(first_point_x, first_point_y), (last_point_x, last_point_y)]
-		x_obs = circle_obstacle[0]
-		y_obs = circle_obstacle[1]
-		r_obs = circle_obstacle[2]
 		r_car = 0.25
-		if x_obs < - math.fabs(0.5*y_obs) and math.fabs(y_obs) <  r_car + r_obs + 0.1:
-			return math.sqrt(x_obs**2 + y_obs**2)
+		if middle_point_x < - math.fabs(0.5*middle_point_y) and math.fabs(middle_point_y) <  r_car + 0.2 and ((middle_point_x**2 + middle_point_y**2) < (r_car + 0.5)**2 or (first_point_x**2 + first_point_y**2) < (r_car + 0.5)**2 or (last_point_x**2 + last_point_y**2) < (r_car + 0.5)**2):
+			return True
+		else:
+			False
 		
 
 if __name__ == "__main__":
