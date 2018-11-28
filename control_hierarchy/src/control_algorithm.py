a73553dcd2671310e0aee6dbf6e79221fe634e7b
jonathan <jnthnj@kth.se>
2018-11-28 12:01:34 +0100

    Built new pure pursuit.

--- control_hierarchy/src/control_algorithm.py
+++ control_hierarchy/src/control_algorithm.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import rospy
+import math
 from abc import ABCMeta, abstractmethod
+from tf.transformations import euler_from_quaternion
+
 
 # Abstract class for handling several controllers
 class ControlAlgorithm:
@@ -12,9 +14,35 @@ class ControlAlgorithm:
         target is whatever the current algorithm needs as input:
             - a Path for pure pursuit
             - a Waypoint for PID
-        constraints is future-proofing for any obstacles that the ALGORITHM needs. Will be given as a list of Obstacles.
+        parameteters is a dictionary tailored to the algorithm.
             
     '''
     @abstractmethod
-    def get_control(self, car_state, target, constraints=None):
+    def get_control(self, car_state, target, parameters=None):
         pass
+
+    '''
+    point_a, point_b: (x,y,k) float tuples
+    
+    returns: float: the distance between the points
+    '''
+    def point_distance(self, point_a, point_b):
+        dx = point_a[0]-point_b[0]
+        dy = point_a[1]-point_b[1]
+
+        return math.sqrt(dx**2 + dy**2)
+
+    '''
+    Takes in a Pose message as the state.
+
+    Returns a dictionary {'x', 'y', 'yaw'} of the same state.
+    '''
+
+    def pose_to_xy_yaw(self, state):
+        x = state.pose.position.x
+        y = state.pose.position.y
+        orientation_q = state.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        yaw = euler_from_quaternion(orientation_list)
+
+        return {'x': x, 'y': y, 'yaw': yaw}
